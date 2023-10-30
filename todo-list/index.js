8ea1774bc4084f858db0508e2098d47b89a988d6
khaankoo <khangerelbeochir@gmail.com>
2023-10-30 14:43:02 +0800

    push

---
+++ todo-list/index.js
@@ -0,0 +1,8 @@
+let addCard = document.querySelectorAll('.add-btn');
+let addTask = document.querySelectorAll('.add-task');
+
+function buttonAdd (){
+    addCard = addTask
+}
+
+addCard[1].onclick = buttonAdd
