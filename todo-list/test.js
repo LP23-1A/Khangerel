70f44f38f64ea21ba9f08aeadce7e5c4744cb089
khaankoo <khangerelbeochir@gmail.com>
2023-11-02 12:44:51 +0800

    zas

---
+++ todo-list/test.js
@@ -0,0 +1,12 @@
+// function print(test) {
+//     console.log('hello ' + test);
+// }
+// print("bat")
+
+
+// function print(text) {
+//     return "hi " + text
+// } 
+// console.log(print('bat'));
+
+function
