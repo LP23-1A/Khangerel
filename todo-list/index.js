27d02ab1f31da9919cb72d80f4f4d1f802e78943
khaankoo <khangerelbeochir@gmail.com>
2023-11-06 14:40:00 +0800

    include

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,6 +1,7 @@
 let addCards = document.querySelectorAll(".add-btn");
 let addTask = document.querySelector(".modal");
 let editbtn = document.querySelectorAll('.write-btn');
+const [datas,setDatas]=useState("")
 
 function openModal() {
     addTask.style.opacity = 1;
@@ -27,12 +28,22 @@ for(let i = 0; i < cancel.length; i++){
 
 
 const data = [];
-function render(data){
-    const cards = document.getElementsByClassName("cardcontainer")[0];
-    const done = []
-    cards.innerHTML = ""
+function render(){
+    const cards = document.getElementsByClassName("cardcontainer");
     for (let i = 0; i < data.length; i++) {
-        cards.innerHTML += createCard(data[i]);
+        if (data[i].status === "todo"){
+            cards[0].innerHTML = "";
+            cards[0].innerHTML += createCard(data[i]);
+        } else if (data[i].status === "inprogress"){
+            cards[1].innerHTML = "";
+            cards[1].innerHTML += createCard(data[i]);
+        } else if (data[i].status === "stuck"){
+            cards[2].innerHTML = "";
+            cards[2].innerHTML += createCard(data[i]);
+        } else if (data[i].status === "done"){
+            cards[3].innerHTML = "";
+            cards[3].innerHTML += createCard(data[i]);
+        }
     }
 }
 function addCard() {
@@ -50,28 +61,33 @@ function addCard() {
         mockData.desc = textarea.value
         mockData.status = status.value
         mockData.priority = priority.value
-        console.log(mockData);
-        data.push(mockData);
+        data.push(...mockData);
         render(data)
 }
 function createCard(card){
     const { title, desc, priority } = card;
     return `
-<div class="card-items flex">
-    <div class="start flex">
-        <div class="done">
-            <button class="done-btn btn"><img src="./img/correct.png" alt="" height="15px" width="15px"></button>
-         </div>
-         <div class="write-task">
-            <h1>${title}</h1>
-            <p>${desc}</p>
-            <div class="priority">${priority}</div>
+    <div class="card-items flex">
+        <div class="start flex">
+            <div class="done">
+                <button class="done-btn btn"><img src="./img/correct.png" alt="" height="15px" width="15px"></button>
+            </div>
+            <div class="write-task">
+                <h1>${title}</h1>
+                <p>${desc}</p>
+                <div class="priority">${priority}</div>
+            </div>
         </div>
+        <div class="end flex">
+            <button class="cancel-btn btn">X</button>
+            <button class="write-btn btn"><img src="./img/write.png" alt="" height="15px" width="15px">
+        </button>
     </div>
-    <div class="end flex">
-        <button class="cancel-btn btn">X</button>
-        <button class="write-btn btn"><img src="./img/write.png" alt="" height="20px" width="20px">
-    </button>
-</div>`
+  
+    
+    `
+
 }
 render(data)
+
+
