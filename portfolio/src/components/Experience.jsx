84daa127af962935dcdc9bf8fea36b6a1cc7a3ea
Khangerel Be-ochir <23LP5567@G303-CC0144.local>
2023-11-28 14:10:55 +0800

    a

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -8,24 +8,6 @@ const Experience = () => {
             <div className="content--body flex column">
                 <div className="upwork flex flex-col gap-16 px-20 w-full">
                     <Expdata />
-                {/* {data.map((props) => {
-                    return(
-                        <div className="upwork-details flex justify-between mx-auto w-full bg-white rounded-xl p-5 drop-shadow-xl max-md:flex-col max-md:gap-10">
-                            <img className="upicon h-10 max-md:h-8 max-md:w-24" src={props.url} alt="" />
-                            <div className="flex gap-3 max-md:flex-col-reverse">
-                                <div className="updesc flex flex-col gap-3">
-                                    <h1 className="text-slate-950 gap-14 text-3xl">{props.h1}</h1>
-                                    <ul className=" flex flex-col gap-2 text-slate-950">
-                                        <li><p>{props.p}</p></li>
-                                        <li><p>{props.p1}</p></li>
-                                        <li><p>{props.p2}</p></li>
-                                    </ul>
-                                </div>
-                                <p className="text-slate-950">{el.day}</p>
-                            </div>
-                        </div>
-                    )
-                })} */}
                 </div>
             </div>
         </div>
--- portfolio/src/components/expdata.jsx
+++ portfolio/src/components/expdata.jsx
@@ -26,20 +26,20 @@ const Expdata = () => {
     return(
         <>
         {
-            data.map((el) => {
+            data.map((props) => {
                 return(
                     <div className="upwork-details flex justify-between mx-auto w-full bg-white rounded-xl p-5 drop-shadow-xl max-md:flex-col max-md:gap-10">
-                        <img className="upicon h-10 max-md:h-8 max-md:w-24" src={el.url} alt="" />
+                        <img className="upicon h-10 max-md:h-8 max-md:w-24" src={props.url} alt="" />
                         <div className="flex gap-3 max-md:flex-col-reverse">
                             <div className="updesc flex flex-col gap-3">
-                                <h1 className="text-slate-950 gap-14 text-3xl">{el.h1}</h1>
+                                <h1 className="text-slate-950 gap-14 text-3xl">{props.h1}</h1>
                                 <ul className=" flex flex-col gap-2 text-slate-950">
-                                    <li><p>{el.p}</p></li>
-                                    <li><p>{el.p1}</p></li>
-                                    <li><p>{el.p2}</p></li>
+                                    <li><p>{props.p}</p></li>
+                                    <li><p>{props.p1}</p></li>
+                                    <li><p>{props.p2}</p></li>
                                 </ul>
                             </div>
-                            <p className="text-slate-950">{el.day}</p>
+                            <p className="text-slate-950">{props.day}</p>
                         </div>
                     </div>
                 )
