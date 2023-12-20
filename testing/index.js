d1b84cbb4f8b208839998674148daa6a8fb00ff2
Khangerel Be-ochir <23LP5567@G303-CC0126.local>
2023-12-20 13:10:59 +0800

    mongodb

--- testing/index.js
+++ testing/index.js
@@ -1,70 +1,57 @@
 import express from "express";
 import bp from "body-parser";
+import mongoose from "mongoose";
+import Url from "./schema/Url.js"
 
 const app = express();
 const PORT = 8000;
+const MONGODB_URI = "mongodb+srv://khangerelbeochir:khangerel123api@cluster0.qiroq0k.mongodb.net/?retryWrites=true&w=majority";
+
 app.use(bp.json());
-const users = [
-  {
-    id: 1,
-    name: "khaankoo",
-  },
-  {
-    id: 2,
-    name: "naki",
-  },
-  {
-    id: 3,
-    name: "darga",
-  },
-  {
-    id: 4,
-    name: "darggagaa",
-  },
-  {
-    id: 5,
-    name: "daldaga",
-  },
-  {
-    id: 6,
-    name: "daravgar",
-  },
-];
 
-app.get("/", (req, res) => {
-  res.send({ success: true, users: users }).end();
+
+
+app.get("/", async (req, res) => {
+  const ress = await Url.find();
+
+  res.send({ success: true, ress }).end();
 });
 
-app.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const filteredData = users.filter((user) => user.id === parseInt(id));
+// app.get("/:id", (req, res) => {
+//   const id = req.params.id;
+//   const filteredData = users.filter((user) => user.id === parseInt(id));
 
-  res.send({ success: true, users: filteredData }).end();
-});
+//   res.send({ success: true, users: filteredData }).end();
+// });
 
-app.post("/", (req, res) => {
-  const data = req.body;
-  users.push(data);
-  res.send({ success: true, users: users }).end();
+app.post("/", async (req, res) => {
+  const newUrl = await Url.create(req.body);
+
+  res.send({ success: true, urls:newUrl }).end();
 });
-app.put("/:id", (req, res) => {
-  const id = req.params.id;
-  users.map((user) => {
-    if (user.id === parseInt(id)) {
-      console.log(id);
-      user.name = req.body.name;
-    }
-  });
-  res.send({ success: true, users: users }).end();
-});
-app.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  const deletedUserId = users.findIndex((user) => user.id === parseInt(id));
-  if (deletedUserId !== -1) {
-    users.splice(deletedUserId, 1);
+// app.put("/:id", (req, res) => {
+//   const id = req.params.id;
+//   users.map((user) => {
+//     if (user.id === parseInt(id)) {
+//       console.log(id);
+//       user.name = req.body.name;
+//     }
+//   });
+//   res.send({ success: true, users: users }).end();
+// });
+// app.delete("/:id", (req, res) => {
+//   const id = req.params.id;
+//   const deletedUserId = users.findIndex((user) => user.id === parseInt(id));
+//   if (deletedUserId !== -1) {
+//     users.splice(deletedUserId, 1);
+//   }
+//   res.send({ success: true, users: users }).end();
+// });
+app.listen(PORT, async () => {
+  try {
+    await mongoose.connect(MONGODB_URI)
+  } catch (error) {
+    console.log(error);
   }
-  res.send({ success: true, users: users }).end();
-});
-app.listen(PORT, () => {
   console.log("Server running");
 });
