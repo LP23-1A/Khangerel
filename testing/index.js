4893baf8d6e51a9ba4d39dbabf12ea1e1865b686
Khangerel Be-ochir <23LP5567@G303-CC0126.local>
2023-12-20 09:55:35 +0800

    api

--- testing/index.js
+++ testing/index.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import bp from "body-parser"
+import express from "express";
+import bp from "body-parser";
 
 const app = express();
 const PORT = 8000;
@@ -7,52 +7,64 @@ app.use(bp.json());
 const users = [
   {
     id: 1,
-    name: 'khaankoo'
+    name: "khaankoo",
   },
   {
     id: 2,
-    name: 'naki'
+    name: "naki",
   },
   {
     id: 3,
-    name: 'darga'
-  }
-]
+    name: "darga",
+  },
+  {
+    id: 4,
+    name: "darggagaa",
+  },
+  {
+    id: 5,
+    name: "daldaga",
+  },
+  {
+    id: 6,
+    name: "daravgar",
+  },
+];
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({ success: true, users: users }).end();
-})
+});
 
-app.get('/:id', (req, res) => {
+app.get("/:id", (req, res) => {
   const id = req.params.id;
-  const filteredData = users.filter((user) => user.id === parseInt(id))
+  const filteredData = users.filter((user) => user.id === parseInt(id));
 
-  res.send({ success: true, users: filteredData}).end();
-})
+  res.send({ success: true, users: filteredData }).end();
+});
 
-app.post('/', (request, response) => {
-  const data = request.body;
+app.post("/", (req, res) => {
+  const data = req.body;
   users.push(data);
-  response.send({ success: true, users: users }).end();
- });
- app.put('/:id', (request, response) => {
-  const id = request.params.id;
+  res.send({ success: true, users: users }).end();
+});
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
   users.map((user) => {
-  if (user.id === parseInt(id)) {
-  console.log(id);
-  user.name = request.body.name;
-  }
+    if (user.id === parseInt(id)) {
+      console.log(id);
+      user.name = req.body.name;
+    }
   });
-  response.send({ success: true, users: users }).end();
- });
- app.delete('/:id', (request, response) => {
-  const id = request.params.id;
+  res.send({ success: true, users: users }).end();
+});
+app.delete("/:id", (req, res) => {
+  const id = req.params.id;
   const deletedUserId = users.findIndex((user) => user.id === parseInt(id));
   if (deletedUserId !== -1) {
-  users.splice(deletedUserId, 1);
+    users.splice(deletedUserId, 1);
   }
-  response.send({ success: true, users: users }).end();
- });
- app.listen(PORT, () => {
-  console.log('Server running');
- });
+  res.send({ success: true, users: users }).end();
+});
+app.listen(PORT, () => {
+  console.log("Server running");
+});
